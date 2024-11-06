d04a02a727f8848369bf294d14312df30c97a1e4
KuvarzinMA <118750083+KuvarzinMA@users.noreply.github.com>
2024-11-06 10:20:41 +0500

    Update time.cpp

--- lab3f/time.cpp
+++ lab3f/time.cpp
@@ -1,88 +1,9 @@
+#include "time.h"
 #include <iostream>
 #include <string>
-#include <iomanip>
-#include <sstream>
-#include <vector>
 using namespace std;
 
 
-
-// Класс Time
-class Time {
-private:
-    int seconds; // общее количество секунд с начала суток
-
-    // Метод для нормализации значения секунд в пределах суток, чтобы всегда оставаться в пределах одного дня
-    void normalize() {
-        seconds %= 86400; // 86400 секунд в сутках
-        if (seconds < 0) {
-            seconds += 86400;
-        }
-    }
-
-public:
-    // Конструктор по умолчанию
-    Time() : seconds(0) {}
-
-    // Конструктор с количеством секунд с начала суток
-    Time(int sec) : seconds(sec) {
-        normalize();
-    }
-
-    // Конструктор с указанием часов, минут и секунд
-    Time(int hours, int minutes, int sec) {
-        seconds = hours * 3600 + minutes * 60 + sec;
-        normalize();
-    }
-
-    // Метод для получения текущего времени в формате ЧЧ:ММ:СС
-    std::string getTextFormat() const {
-        int hours = (seconds / 3600) % 24;
-        int minutes = (seconds % 3600) / 60;
-        int sec = seconds % 60;
-        // создает объект oss, который представляет собой поток вывода, работающий со строками
-        std::ostringstream oss;
-        oss << std::setw(2) << std::setfill('0') << hours << ":"
-            << std::setw(2) << std::setfill('0') << minutes << ":"
-            << std::setw(2) << std::setfill('0') << sec;
-        return oss.str();
-    }
-
-    // Вывод текущего времени в формате ЧЧ:ММ:СС
-    void print() const {
-        std::cout << "Текущее время: " << getTextFormat() << std::endl;
-    }
-
-    // Метод для получения текущего часа
-    int getCurrentHour() const {
-        return (seconds / 3600) % 24;
-    }
-
-    // Метод для получения минут с начала текущего часа
-    int getMinutesFromCurrentHour() const {
-        return (seconds % 3600) / 60;
-    }
-
-    // Метод для получения секунд с начала текущей минуты
-    int getSecondsFromCurrentMinute() const {
-        return seconds % 60;
-    }
-
-    // Метод для получения общего количества часов
-    int getTotalHours() const {
-        return seconds / 3600;
-    }
-
-    // Метод для получения общего количества минут
-    int getTotalMinutes() const {
-        return seconds / 60;
-    }
-
-    // Метод для получения общего количества секунд
-    int getTotalSeconds() const {
-        return seconds;
-    }
-};
 
 
 // Главная функция
@@ -120,7 +41,7 @@ int main() {
     std::cout << "Секунд соответствуют времени 123: " << task3.getTotalSeconds() << "\n" << std::endl;
 
 
-    
+
 
     return 0;
 }
