9f6c229ccb0d4297ede270fb2f00b60cf9a3129f
KuvarzinMA <118750083+KuvarzinMA@users.noreply.github.com>
2024-11-06 10:18:42 +0500

    Update name.cpp

--- lab3f/name.cpp
+++ lab3f/name.cpp
@@ -1,41 +1,4 @@
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <sstream>
-#include <vector>
-using namespace std;
-
-// Класс Name
-class Name {
-private:
-    string surname;      // Фамилия
-    string first_name;   // Имя
-    string patronymic;   // Отчество
-
-public:
-    // Конструктор с параметрами по умолчанию
-    Name(string s = "", string f = "", string p = "") : surname(s), first_name(f), patronymic(p) {}
-
-    // Метод для вывода в строковом виде
-    void print() const {
-        if (!surname.empty()) cout << surname;
-        if (!first_name.empty()) cout << (surname.empty() ? "" : " ") << first_name;
-        if (!patronymic.empty()) cout << ((surname.empty() && first_name.empty()) ? "" : " ") << patronymic;
-        cout << endl;
-    }
-
-    // Преобразование к строковому виду
-    string to_string() const {
-        string result;
-        if (!surname.empty()) result += surname;
-        if (!first_name.empty()) result += (result.empty() ? "" : " ") + first_name;
-        if (!patronymic.empty()) result += (result.empty() ? "" : " ") + patronymic;
-        return result;
-    }
-};
-
-
-// Главная функция
+#include "name.h"
 int main() {
     setlocale(LC_ALL, "RUSSIAN");
 
